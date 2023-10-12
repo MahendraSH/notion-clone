0355162fe51a557c35c38d71380374d70943b360
MahendraSH <maahi2001sh@gmail.com>
2023-10-12 16:57:56 +0530

    added ui.shadcn  ,and auth using clerk

--- app/(auth)/(routers)/layout.tsx
+++ app/(auth)/(routers)/layout.tsx
@@ -1,11 +1,21 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "notion clone Sigin ",
+  description: "notion clone  ",
+};
 import { FC } from "react";
 
-interface layoutProps {
+interface AuthlayoutProps {
   children: React.ReactNode;
 }
 
-const layout: FC<layoutProps> = ({ children }) => {
-  return <div> {children}</div>;
+const Authlayout: FC<AuthlayoutProps> = ({ children }) => {
+  return (
+    <div className=" flex flex-col items-center justify-center  w-full h-full">
+      {children}
+    </div>
+  );
 };
 
-export default layout;
+export default Authlayout;
