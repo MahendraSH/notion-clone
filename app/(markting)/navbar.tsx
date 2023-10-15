e32dee8fd32e6cd92d8f7ea2edf66a1f9f871b47
MahendraSH <maahi2001sh@gmail.com>
2023-10-15 07:58:33 +0530

    auth and backend using convex and cleck auth

--- app/(markting)/navbar.tsx
+++ app/(markting)/navbar.tsx
@@ -5,11 +5,17 @@ import { cn } from "@/lib/utils";
 import { FC } from "react";
 import Logo from "./_components/logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, SignUpButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import LoaderSpiner from "@/components/loader-spiner";
+import Link from "next/link";
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = ({}) => {
   const scrolled = useScrollTop();
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
     <div
       className={cn(
@@ -19,6 +25,26 @@ const Navbar: FC<NavbarProps> = ({}) => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <LoaderSpiner size={"sm"} />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant={"ghost"}>Login</Button>
+            </SignInButton>
+
+            <SignUpButton mode="modal">
+              <Button variant={"ghost"}>Get Notion free</Button>
+            </SignUpButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant={"ghost"} size={"sm"} asChild>
+              <Link href={"/doucments"}>Enter notion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
---
+++ components/loader-spiner.tsx
@@ -0,0 +1,29 @@
+import { cn } from "@/lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+import { Loader } from "lucide-react";
+import { FC } from "react";
+
+const LoaderSpinerVariants = cva("text-muted-foreground animate-spin ", {
+  variants: {
+    size: {
+      default: "h-4 w-4",
+      sm: "h-3 w-3",
+      lg: "h-6 w-6",
+      icon: "h-10 w-10",
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  },
+});
+
+interface LoaderSpinerProps extends VariantProps<typeof LoaderSpinerVariants> {}
+const LoaderSpiner: FC<LoaderSpinerProps> = ({ size }) => {
+  return (
+    <>
+      <Loader className={cn(LoaderSpinerVariants({ size }))} />
+    </>
+  );
+};
+
+export default LoaderSpiner;
